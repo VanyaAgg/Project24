60c449606f56c3a34fb813d71bb4c2adacc23237
VanyaAgg <65060618+VanyaAgg@users.noreply.github.com>
2020-10-12 13:14:06 +0530

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,16 @@
+class Bin {
+    constructor(x,y,width,height){
+        this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
+        this.w = width;
+        this.h = height;
+        World.add (world,this.body);
+console.log(this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        fill("red");
+        rect(pos.x,pos.y,this.w,this.h);
+
+
+    }
+} 
